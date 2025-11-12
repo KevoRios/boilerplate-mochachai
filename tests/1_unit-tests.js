1a186bf90bbad42928c71aebe3de5935e7fd27eb
KevoRios <kevingrios47@gmail.com>
2025-11-12 17:24:41 -0300

    Update 1_unit-tests.js

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -77,12 +77,12 @@ suite('Unit Tests', function () {
 
   suite('Comparisons', function () {
     // #8
-  test('#isAbove, #isAtMost', function () {
-    assert.isAbove('apple'.length, 3, 'string length is greater than 3');
-    assert.isAtMost(6, 19, '6 is at most 19');
-    assert.isAbove(2 * Math.random(), 0, 'generated number is positive');
-    assert.isAtMost(5 % 2, 2, 'remainder of 5/2 is at most 2');
-    assert.isAtMost(2 + 2, 5, 'sum of 2 and 2 is at most 5');
+test('#isAbove, #isAtMost', function () {
+  assert.isAbove('apple'.length, 3, 'string length is greater than 3');
+  assert.isAtMost(6, 19, '6 is at most 19');
+  assert.isAbove(2 * Math.random(), 0, 'generated number is positive');
+  assert.isAtMost(5 % 2, 2, 'remainder of 5/2 is at most 2');
+  assert.isAtMost(2 + 2, 5, 'sum of 2 and 2 is at most 5');
 });
     // #9
     test('#isBelow, #isAtLeast', function () {

5171a7c4508392e720110e4831340ea5552ac5c8
KevoRios <kevingrios47@gmail.com>
2025-11-13 18:57:21 -0300

    Update 1_unit-tests.js

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -125,6 +125,7 @@ suite('Comparisons', function () {
     // #15
     test('#match, #notMatch', function () {
       const regex = /#\sname\:\s[\w\s]+,\sage\:\s\d+\s?$/;
+
       assert.match(formatPeople('John Doe', 35), regex);
       assert.notMatch(formatPeople('Paul Smith III', 'twenty-four'), regex);
     });

e68287785585ed971871f0f93969e86d8d593cf8
KevoRios <kevingrios47@gmail.com>
2025-11-14 16:50:03 -0300

    Update 2_functional-tests.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -8,62 +8,66 @@ chai.use(chaiHttp);
 
 suite('Functional Tests', function () {
   this.timeout(5000);
+
   suite('Integration tests with chai-http', function () {
+
     // #1
-test('Test GET /hello with no name', function (done) {
-  chai
-    .request(server)
-    .keepOpen()
-    .get('/hello')             // SIN query string
-    .end(function (err, res) {
-      assert.equal(res.status, 200);
-      assert.equal(res.text, 'hello Guest');
-      done();
+    test('Test GET /hello with no name', function (done) {
+      chai
+        .request(server)
+        .get('/hello')
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello Guest');
+          done();
+        });
     });
- });
-test('Test GET /hello with your name', function (done) {
-  chai
-    .request(server)
-    .keepOpen()
-    .get('/hello?name=xy_z')
-    .end(function (err, res) {
-      assert.equal(res.status, 200);
-      assert.equal(res.text, 'hello xy_z');
-      done();
-    });
+
+    // #2
+    test('Test GET /hello with your name', function (done) {
+      chai
+        .request(server)
+        .get('/hello?name=xy_z')
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello xy_z');
+          done();
+        });
     });
 
     // #3
-test('Send {surname: "Colombo"}', function (done) {
-  chai
-    .request(server)
-    .put('/travellers')
-    .send({ surname: 'Colombo' })
-    .end(function (err, res) {
-      assert.equal(res.status, 200);
-      assert.equal(res.type, 'application/json');
-      assert.equal(res.body.name, 'Cristoforo');
-      assert.equal(res.body.surname, 'Colombo');
-      
-      done();
+    test('Send {surname: "Colombo"}', function (done) {
+      chai
+        .request(server)
+        .put('/travellers')
+        .send({ surname: 'Colombo' })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.type, 'application/json');
+          assert.equal(res.body.name, 'Cristoforo');
+          assert.equal(res.body.surname, 'Colombo');
+          done();
+        });
     });
-});
-       // #4
-    test('Send {surname: "da Verrazzano"}', function (done) {
-       chai
-    .request(server)
-    .put('/travellers')
-    .send({ surname: 'da Verrazzano' })
-    .end(function (err, res) {
-      assert.equal(res.status, 200);
-      assert.equal(res.type, 'application/json');
-      assert.equal(res.body.name, 'Giovanni');
-      assert.equal(res.body.surname, 'da Verrazzano');
 
-      done();
+    // #4
+    test('Send {surname: "da Verrazzano"}', function (done) {
+      chai
+        .request(server)
+        .put('/travellers')
+        .send({ surname: 'da Verrazzano' })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.type, 'application/json');
+          assert.equal(res.body.name, 'Giovanni');
+          assert.equal(res.body.surname, 'da Verrazzano');
+          done();
+        });
     });
+
   });
 });
+
 
 const Browser = require('zombie');
 

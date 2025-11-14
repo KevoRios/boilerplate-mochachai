8a9b28921d2df6ae101d45d71da0ca35b66d34d4
KevoRios <kevingrios47@gmail.com>
2025-11-14 16:42:18 -0300

    Update 2_functional-tests.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -47,23 +47,24 @@ test('Send {surname: "Colombo"}', function (done) {
       
       done();
     });
-
 });
-    // #4
-    test('send {surname: "da Verrazzano"}', function (done) {
-      chai
+       // #4
+    test('Send {surname: "da Verrazzano"}', function (done) {
+       chai
     .request(server)
     .put('/travellers')
     .send({ surname: 'da Verrazzano' })
     .end(function (err, res) {
-      assert.equal(res.status, 200, 'response status should be 200');
-      assert.equal(res.type, 'application/json', 'Response should be json');
+      assert.equal(res.status, 200);
+      assert.equal(res.type, 'application/json');
       assert.equal(res.body.name, 'Giovanni');
       assert.equal(res.body.surname, 'da Verrazzano');
+
       done();
     });
-}); 
+  });
 });
+
 const Browser = require('zombie');
 
 

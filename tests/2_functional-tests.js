8f922da73c9b0d8871c279b88e732e245923e980
KevoRios <kevingrios47@gmail.com>
2025-11-14 17:11:08 -0300

    Update 2_functional-tests.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -70,7 +70,7 @@ suite('Functional Tests', function () {
 
 
 const Browser = require('zombie');
-Browser.site = 'http://0.0.0.0:3000/';
+Browser.site = 'http://0.0.0.0:3000';
 
 
 suite('Functional Tests with Zombie.js', function () {

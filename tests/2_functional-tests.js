490eb60331930521cf1fee8c2585096057f9fa6b
KevoRios <kevingrios47@gmail.com>
2025-11-14 10:48:22 -0300

    Update 2_functional-tests.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -32,18 +32,21 @@ test('Test GET /hello with your name', function (done) {
       done();
     });
     });
+
     // #3
-    test('Send {surname: "Colombo"}', function (done) {
-      chai
-        .request(server)
-        .keepOpen()
-        .put('/travellers')
-
-        .end(function (err, res) {
-          assert.fail();
-
-          done();
-        });
+test('Send {surname: "Colombo"}', function (done) {
+  chai
+    .request(server)
+    .keepOpen()
+    .put('/travellers')
+    .send({ surname: 'Colombo' })
+    .end(function (err, res) {
+      assert.equal(res.status, 200);
+      assert.equal(res.type, 'application/json');
+      assert.equal(res.body.name, 'Cristoforo');
+      assert.equal(res.body.surname, 'Colombo');
+      done();
+    });
     });
     // #4
     test('Send {surname: "da Verrazzano"}', function (done) {

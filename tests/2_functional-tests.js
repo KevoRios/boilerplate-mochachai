a43f53361de9e7160f9e9903e45de75fc0ecfde3
KevoRios <kevingrios47@gmail.com>
2025-11-14 17:26:15 -0300

    Update 2_functional-tests.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -70,7 +70,7 @@ suite('Functional Tests', function () {
 
 
 const Browser = require('zombie');
-Browser.site = 'http://0.0.0.0:3000';
+Browser.site = 'https://boilerplate-mochachai-2xye.onrender.com';
 
 
 suite('Functional Tests with Zombie.js', function () {
